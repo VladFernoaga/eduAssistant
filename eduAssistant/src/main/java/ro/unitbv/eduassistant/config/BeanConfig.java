b4106918b5cd2176d06949b9015cb16d1cc97671
Vlad Fernoaga <vlad.fernoaga@in-tech.com>
2018-01-18 18:53:29 +0200

    INTEGRATION  SPRING-CHATBOT-DATABASE is WORKING

--- eduAssistant/src/main/java/ro/unitbv/eduassistant/config/BeanConfig.java
+++ eduAssistant/src/main/java/ro/unitbv/eduassistant/config/BeanConfig.java
@@ -1,10 +1,38 @@
 package ro.unitbv.eduassistant.config;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import io.fouad.jtb.core.JTelegramBot;
+import ro.unitbv.eduassistant.chatbot.handler.SimpleUpdateHandler;
 
 @Configuration
 @ComponentScan({"ro.unitbv.eduassistant"})
 public class BeanConfig {
 	
+	/** The Constant LOGGER. */
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	private static final String API_TOKEN = "363877872:AAEGG9_770o2V7y_yVZfy2_zZjpweUp4J8Y";
+	private static final String BOOT_NAME = "easyShopper_bot";
+
+	@Autowired
+	private SimpleUpdateHandler updateHandler;
+	
+	@Bean
+	public CommandLineRunner run() {
+		return (args) -> {
+			LOGGER.info("My chat boot is started!");
+			JTelegramBot bot = new JTelegramBot(BOOT_NAME, API_TOKEN, updateHandler);
+
+			LOGGER.info("Chatboot inistialized - start() will ocure");
+			bot.start();
+
+		};
+	}
 }
